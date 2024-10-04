f4b3c9fd3f9c6286e5ef662cd0db3abe45193b68
unknown <claudiastz825@gmail.com>
2024-10-04 16:29:49 +0100

    packed in a folder

--- project/main.py
+++ project/main.py
@@ -4,11 +4,12 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
 # https://stackoverflow.com/questions/76461596/unable-to-use-selenium-webdriver-getting-two-exceptions
 # This is due to changes in Selenium 4.10
 
-service = Service(executable_path=r"C:\\Users\\Paulinha\\BPCE-IT Exercise\\chromedriver.exe")
+service = Service(executable_path=r"C:\\Users\\Paulinha\\Desktop\\Exercise\\chromedriver.exe")
 
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service)
@@ -22,6 +23,10 @@ wait = WebDriverWait(driver, 5)
 try:
     wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="consent_prompt_submit"]'))).click()
     print('Successfully Accepted Cookies')
+    driver.execute_script("window.scrollTo(0, 900);")
+    print('Successfully Scrolled down')
+    driver.get("https://agences.banquepopulaire.fr/banque-assurance/")
+    print('Successfully Navigated to Find an Agency')
 except:
-    print('Could not click')
+    print('Could not perform automation')
     pass
